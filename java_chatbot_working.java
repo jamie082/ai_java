a6b5edecfed5640c64a32d788a8d79966b239a5e
jamie082 <jamiemorrissey02@gmail.com>
2023-12-12 15:56:48 -0800

    added and to read all four data files

--- java_chatbot_working.java
+++ java_chatbot_working.java
@@ -1,14 +1,18 @@
-import java.util.Scanner;
+import java.util.Scanner;  // import settings specified 
 import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.io.IOException;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.util.Random;
 
+
  class loop {
 
     public static void main(String[] args) {  // type in these questions in the prompt to ask it the questions
-        String[] cannedPhrases = {"Do you want legalization in all 50 states?",   // Democrat
+        String[] cannedPhrases = {"Do you want legalization in all 50 states?",   // Democrat   these are the responses
                                   "What do you want to do about lower taxes for the rich?", // Democrat
                                   "What do you want to do about people who earn higher than $450,000 yearly?",  // Republican
                                   "What do you want to do about universal basic income for democratic states?", // Democrat
@@ -21,11 +25,11 @@ import java.util.Random;
         int rounds = conversationStart.nextInt();
         //conversationStart.nextLine();
         String[] transcript = new String[2 * rounds + 1];
-        transcript[0] = "Sounds great! How are you doing today?";
+        transcript[0] = "Sounds great! How are you doing today?"; // ask "how are you doing today?"
         System.out.println(transcript[0]);
         int index = -1;
         int myHigher = 001, myLower = 002, myFree = 003, myUniversal = 004;
-        for (int i = 1; i <= rounds; i++) { // Main program loop
+        for (int i = 1; i <= rounds; i++) { // Main program loop, loop amount of times specified by input
             String userWords = conversationStart.nextLine();
             String mirrored;
             StringBuilder result = new StringBuilder();
@@ -40,26 +44,34 @@ import java.util.Random;
                 }
                 switch(word) {  // use switch keyword for AI
                     case "lower": // data_1.txt
-                        word = "higher";
+                        word = "Democrat"; // write "higher" to data_1.txt
                         try {
-                            File newTextFile =new File("C:/DATA_JAMIE/higher.txt");
+                            File newTextFile =new File("C:/DATA_JAMIE/higher.txt"); // open higher.txt for writing
                             /* This logic is to create the file if the
                             * file is not already preent */
 
-                            if(!newTextFile.exists()) {
+                            if(!newTextFile.exists()) { // if text file exists, then dont overwrite it
                                 newTextFile.createNewFile();
                             }
 
                             String str = word;
 
                             // Here true is to append the content to file
-                            FileWriter fw = new FileWriter(newTextFile, true);
+                            FileWriter fw = new FileWriter(newTextFile); // append s set to true
                             // BufferedWriter writer give better perforamance
                             BufferedWriter bw = new BufferedWriter(fw);
-                            bw.write(str + "\n");
+                            bw.write(str + "\n"); // write str plus new line 
                             // Closing BufferedWriter stream
-                            bw.close(); 
+                            bw.close();  // close bw
                             System.out.println("Data successfully appened at the end of the file");
+                            // read text file in Java
+                            File myObj = new File("C:/DATA_JAMIE/higher.txt");
+                            Scanner myReader = new Scanner(myObj);
+                            while (myReader.hasNextLine()) {     // read the data
+                                String lower_string = myReader.nextLine();
+                                //System.out.println(data);
+                            }
+                            myReader.close();
                             ++myLower;
                         }
                         catch (Exception e) {
@@ -67,12 +79,9 @@ import java.util.Random;
                         }
                         break;
                     case "higher": // data_2.txt
-                        word = "lower";
+                        word = "Republican"; // write "lower" to lower.txt
                         try {
                             File newTextFile =new File("C:/DATA_JAMIE/lower.txt");
-                            /* This logic is to create the file if the
-                            * file is not already preent */
-
                             if(!newTextFile.exists()) {
                                 newTextFile.createNewFile();
                             }
@@ -80,13 +89,19 @@ import java.util.Random;
                             String str = word;
 
                             // Here true is to append the content to file
-                            FileWriter fw = new FileWriter(newTextFile, true);
+                            FileWriter fw = new FileWriter(newTextFile);
                             // BufferedWriter writer give better perforamance
                             BufferedWriter bw = new BufferedWriter(fw);
                             bw.write(str + "\n");
                             // Closing BufferedWriter stream
                             bw.close(); 
-                            System.out.println("Data successfully appened at the end of the file");
+                            System.out.println("Data successfully appeded at the end of the file");
+                            File myObj2 = new File("C:/DATA_JAMIE/lower.txt");
+                            Scanner myReader = new Scanner(myObj2);
+                            while (myReader.hasNextLine()) {
+                                String higher_string = myReader.nextLine();
+                                //System.out.println(data);
+                            }
                             ++myHigher;
                         }
                         catch (Exception e) {
@@ -96,7 +111,7 @@ import java.util.Random;
                     case "free": // data_3.txt
                         word = "Democrat";
                         try {
-                            File newTextFile =new File("C:/DATA_JAMIE/democrat.txt");
+                            File newTextFile =new File("C:/DATA_JAMIE/democrat.txt"); // open Text file for writing, democrat.txt
                             /* This logic is to create the file if the
                             * file is not already preent */
 
@@ -107,21 +122,27 @@ import java.util.Random;
                             String str = word;
 
                             // Here true is to append the content to file
-                            FileWriter fw = new FileWriter(newTextFile, true);
+                            FileWriter fw = new FileWriter(newTextFile);
                             // BufferedWriter writer give better perforamance
                             BufferedWriter bw = new BufferedWriter(fw);
                             bw.write(str + "\n");
                             // Closing BufferedWriter stream
                             bw.close(); 
                             System.out.println("Data successfully appened at the end of the file");
+                            File myObj2 = new File("C:/DATA_JAMIE/democrat.txt");
+                            Scanner myReader = new Scanner(myObj2);
+                            while (myReader.hasNextLine()) {
+                                String free_string = myReader.nextLine();
+                                //System.out.println(data);
+                            }
                             ++myFree;
                         }
                         catch (Exception e) {
                             e.getStackTrace();
                         }
                         break;
-                    case "marijuana": // data_4.txt
-                        word = "banned";
+                    case "legalization": // data_4.txt
+                        word = "Democrat";
                         try {
                             File newTextFile =new File("C:/DATA_JAMIE/marijuana.txt");
                             /* This logic is to create the file if the
@@ -131,29 +152,35 @@ import java.util.Random;
                                 newTextFile.createNewFile();
                             }
 
-                            String str = word;
+                            String str = word; // use String statement 
 
                             // Here true is to append the content to file
-                            FileWriter fw = new FileWriter(newTextFile, true);
+                            FileWriter fw = new FileWriter(newTextFile);
                             // BufferedWriter writer give better perforamance
                             BufferedWriter bw = new BufferedWriter(fw);
-                            bw.write(str + "\n");
+                            bw.write(str + "\n"); // write to data file 
                             // Closing BufferedWriter stream
-                            bw.close(); 
+                            bw.close();  // close data file 
                             System.out.println("Data successfully appened at the end of the file");
+                            File myObj2 = new File("C:/DATA_JAMIE/marijuana.txt");
+                            Scanner myReader = new Scanner(myObj2);
+                            while (myReader.hasNextLine()) {
+                                String legalization_string = myReader.nextLine();
+                                //System.out.println(data);
+                            }
                             ++myUniversal;
                         }
-                        catch (Exception e) {
+                        catch (Exception e) {  // if exception is caught
                             e.getStackTrace();
                         }
-                        break;
+                        break;   // use break statement
                     }
                 result.append(word);
 
             }
 
             String newVersion = result.toString();
-            if (!newVersion.equals(userWords)) {
+            if (!newVersion.equals(userWords)) { // if else statements
                 mirrored = newVersion;
             }
             else {
